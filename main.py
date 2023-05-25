4bdd4e844bbf7484a7d9cd41481e372ce2dcd8b6
Radyko <radykolev@Radys-MacBook-Pro.local>
2023-05-25 10:30:20 -0500

    Updated version of env code

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ print("hello world")
 
 import yfinance as yahooFinance
 
-# Here We are getting Facebook financial information
-# We need to pass FB as argument for that
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+endpoint = os.getenv("ENDPOINT")
+apiKey = os.getenv("APIKEY")
+secretKey = os.getenv("SECRETKEY")
+
+print(f"The endpoint is {endpoint}, the api key is {apiKey}, the secret key is {secretKey}")
+
+# Here We are getting Amazon financial information
+# We need to pass AMZN as argument for that
 GetAmazonInformation = yahooFinance.Ticker("AMZN")
 
 # whole python dictionary is printed here
@@ -18,6 +28,6 @@ print("Price Earnings Ratio : ", GetAmazonInformation.info['trailingPE'])
 # display Company Beta
 print("Company Beta : ", GetAmazonInformation.info['beta'])
 
-amzn_historical = GetAmazonInformation.history(start="2023-05-23", end="2023-05-24", interval="1m")
-print (amzn_historical)
+amzn_historical = GetAmazonInformation.history(start="2023-05-24", end="2023-05-25", interval="1m")
+print(amzn_historical)
 
