2bee7534a3ec93b0d774f6bf42962cb07d686d1c
SohamGupta21 <sohamgupta21@gmail.com>
2023-05-26 14:31:17 -0500

    made alpaca class

---
+++ alpacaTrader.py
@@ -0,0 +1,43 @@
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
+
+
+class AlpacaTrader:
+    def __init__(self, endpoint, apiKey, secretKey):
+        self.endpoint = endpoint
+        self.apiKey = apiKey
+        self.secretKey = secretKey
+        self.trading_client = TradingClient(apiKey, secretKey, paper=True)
+
+    def buyStock(self, sym, quantity):
+        # Setting parameters for our buy order
+        market_order_data = MarketOrderRequest(
+                              symbol=sym,
+                              qty=quantity,
+                              side=OrderSide.BUY,
+                              time_in_force=TimeInForce.GTC
+                          )
+
+        market_order = self.trading_client.submit_order(market_order_data)
+        for property_name, value in market_order:
+          print(f"\"{property_name}\": {value}")
+
+    def sellStock(self, sym, quantity):
+        # Setting parameters for our buy order
+        market_order_data = MarketOrderRequest(
+                              symbol=sym,
+                              qty=quantity,
+                              side=OrderSide.SELL,
+                              time_in_force=TimeInForce.GTC
+                          )
+
+        market_order = self.trading_client.submit_order(market_order_data)
+        for property_name, value in market_order:
+          print(f"\"{property_name}\": {value}")
+
+    def getAccountInfo(self):
+        account = self.trading_client.get_account()
+        for property_name, value in account:
+          print(f"\"{property_name}\": {value}")
+        
--- main.py
+++ main.py
@@ -1,58 +1,50 @@
-import yfinance as yahooFinance
+# import yfinance as yahooFinance
+# import os
+# import pandas as pd
+
+# from dotenv.main import load_dotenv
+# load_dotenv()
+
+
+# endpoint = os.getenv("ENDPOINT")
+# apiKey = os.getenv("APIKEY")
+# secretKey = os.getenv("SECRETKEY")
+
+# # Here We are getting Amazon financial information
+# # We need to pass AMZN as argument for that
+# GetAmazonInformation = yahooFinance.Ticker("AMZN")
+
+# # whole python dictionary is printed here
+# print(GetAmazonInformation.info)
+
+# # display Company Sector
+# print("Company Sector : ", GetAmazonInformation.info['sector'])
+
+# # display Price Earnings Ratio
+# print("Price Earnings Ratio : ", GetAmazonInformation.info['trailingPE'])
+
+# # display Company Beta
+# print("Company Beta : ", GetAmazonInformation.info['beta'])
+
+# amzn_historical = GetAmazonInformation.history(start="2023-05-24", end="2023-05-25", interval="1m")
+# print(amzn_historical)
 
 import os
-import pandas as pd
-
 from dotenv.main import load_dotenv
-load_dotenv()
+from alpacaTrader import AlpacaTrader
 
-from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import MarketOrderRequest
-from alpaca.trading.enums import OrderSide, TimeInForce
+if __name__ == '__main__':
+    # This code won't run if this file is imported.
+    load_dotenv()
 
-endpoint = os.getenv("ENDPOINT")
-apiKey = os.getenv("APIKEY")
-secretKey = os.getenv("SECRETKEY")
+    alpacaTrader = AlpacaTrader(os.getenv("ENDPOINT"), os.getenv("APIKEY"), os.getenv("SECRETKEY"))
 
-trading_client = TradingClient(apiKey, secretKey, paper=True)
-
-# Setting parameters for our buy order
-# market_order_data = MarketOrderRequest(
-#                       symbol="AMZN",
-#                       qty=1,
-#                       side=OrderSide.SELL,
-#                       time_in_force=TimeInForce.GTC
-#                   )
-#
-# market_order = trading_client.submit_order(market_order_data)
-# for property_name, value in market_order:
-#   print(f"\"{property_name}\": {value}")
+    alpacaTrader.sellStock("AMZN", 4)
+    
 
 
-#
-# account = trading_client.get_account()
-# for property_name, value in account:
-#   print(f"\"{property_name}\": {value}")
-#
-#
-# print(f"The endpoint is {endpoint}, the api key is {apiKey}, the secret key is {secretKey}")
 
-# Here We are getting Amazon financial information
-# We need to pass AMZN as argument for that
-GetAmazonInformation = yahooFinance.Ticker("AMZN")
 
-# whole python dictionary is printed here
-print(GetAmazonInformation.info)
 
-# display Company Sector
-print("Company Sector : ", GetAmazonInformation.info['sector'])
 
-# display Price Earnings Ratio
-print("Price Earnings Ratio : ", GetAmazonInformation.info['trailingPE'])
-
-# display Company Beta
-print("Company Beta : ", GetAmazonInformation.info['beta'])
-
-amzn_historical = GetAmazonInformation.history(start="2023-05-24", end="2023-05-25", interval="1m")
-print(amzn_historical)
 
