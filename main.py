02dee3f0ffb89b5058da964cc345c4890f6dc10a
Radyko <radykolev@Radys-MacBook-Pro.local>
2023-06-20 09:57:35 -0500

    Rady updated code

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ if __name__ == '__main__':
     # This code won't run if this file is imported.
     # schedule to run this function every 5 seconds
 
-    schedule.every(0.5).seconds.do(tradingAlgos.simpleMomentumTrader)
+    schedule.every(0.5).seconds.do(tradingAlgos.simpleMomentumTraderWithStopLoss)
 
 
 
     while True:
         # Run any pending jobs
         schedule.run_pending()
+
--- tradingAlgos.py
+++ tradingAlgos.py
@@ -1,12 +1,17 @@
 from alpacaTrader import AlpacaTrader
 from indicators import indicators
+import os
 
 class TradingAlgos():
     def __init__(self):
         self.last_price = 0
         self.current_price = 0
-        self.alpacaTrader = AlpacaTrader("https://paper-api.alpaca.markets", "PKCZKBUQHLTN83I82B4U", "0VuY0Fe9dUHffs1kKPUZMDsEJqouKCoocgztgglZ")
+        self.alpacaTrader = AlpacaTrader("https://paper-api.alpaca.markets", "PKI5BEQA5IIUNH6YV9K5", "sACQ8jXfOdEjJ90ZtC70XJjl25hmQs82vQSzZ3BN")
         self.indicators = indicators()
+        self.shares = 0
+
+
+
 
     def simpleMomentumTrader(self):
         self.last_price = self.current_price
@@ -22,6 +27,23 @@ class TradingAlgos():
                 # sell code
                 print("sell")
                 self.alpacaTrader.sellStock('AMZN' , 1)
+
+    def simpleMomentumTraderWithStopLoss(self):
+        self.last_price = self.current_price
+        self.current_price = indicators.get_current_price(indicators, "AMZN")
+
+        if not self.last_price == 0:
+            if self.current_price > self.last_price:
+                # buy code
+                print("buy")
+                self.alpacaTrader.buyStock('AMZN' , 1)
+                self.shares +=1
+
+            elif self.current_price < self.last_price and self.shares > 0:
+                # sell code
+                print("sell")
+                self.alpacaTrader.sellStock('AMZN' , self.shares)
+                self.shares = 0
 
     def rsiTrader(self):
         gains = []
