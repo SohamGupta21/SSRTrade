02b85ac9a1d0967cfb7715899c174072a1d0658e
Radyko <radykolev@Radys-MacBook-Pro.local>
2023-06-21 10:58:31 -0500

    Rady edited the stochastic

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import schedule
 from tradingAlgos import TradingAlgos
 
 
-tradingAlgos = TradingAlgos()
+tradingAlgos = TradingAlgos("AMZN")
 
 if __name__ == '__main__':
     # This code won't run if this file is imported.
     # schedule to run this function every 5 seconds
 
-    schedule.every(0.5).seconds.do(tradingAlgos.stochastic)
+    schedule.every(1.5).seconds.do(tradingAlgos.tripleThreatTrader)
 
 
 
--- tradingAlgos.py
+++ tradingAlgos.py
@@ -4,54 +4,65 @@ import os
 import numpy as np
 
 class TradingAlgos():
-    def __init__(self):
+    def __init__(self, tick):
+        self.ticker = tick
         self.last_price = 0
         self.current_price = 0
-        self.alpacaTrader = AlpacaTrader("https://paper-api.alpaca.markets", "PKI5BEQA5IIUNH6YV9K5", "sACQ8jXfOdEjJ90ZtC70XJjl25hmQs82vQSzZ3BN")
+        self.alpacaTrader = AlpacaTrader("https://paper-api.alpaca.markets", "PK1VFVS6OA1V6DJ7BB6Z", "crQXqJpBrHdiSwDXxK9hLiv2eZTDi239JdRJ6Hrb")
         self.indicators = indicators()
         self.shares = 0
 
         self.rsiData = []
         self.macdData = []
         self.stochasticData = []
+        self.macdHistory = []
+
+        self.macdSignalLine = 0
+        self.macd = 0
+        self.rsi = 0
+        self.stochasticK = 0
+
+        self.tripleThreatCounter = 0
+
+
 
 
 
 
     def simpleMomentumTrader(self):
         self.last_price = self.current_price
-        self.current_price = indicators.get_current_price(indicators, "AMZN")
+        self.current_price = indicators.get_current_price(indicators, self.ticker)
 
         if not self.last_price == 0:
             if self.current_price > self.last_price:
                 # buy code
                 print("buy")
-                self.alpacaTrader.buyStock('AMZN' , 1)
+                self.alpacaTrader.buyStock(self.ticker , 1)
 
             elif self.current_price < self.last_price:
                 # sell code
                 print("sell")
-                self.alpacaTrader.sellStock('AMZN' , 1)
+                self.alpacaTrader.sellStock(self.ticker , 1)
 
     def simpleMomentumTraderWithStopLoss(self):
         self.last_price = self.current_price
-        self.current_price = indicators.get_current_price(indicators, "AMZN")
+        self.current_price = indicators.get_current_price(indicators, self.ticker)
 
         if not self.last_price == 0:
             if self.current_price > self.last_price:
                 # buy code
                 print("buy")
-                self.alpacaTrader.buyStock('AMZN' , 1)
+                self.alpacaTrader.buyStock(self.ticker , 1)
                 self.shares +=1
 
             elif self.current_price < self.last_price and self.shares > 0:
                 # sell code
                 print("sell")
-                self.alpacaTrader.sellStock('AMZN' , self.shares)
+                self.alpacaTrader.sellStock(self.ticker , self.shares)
                 self.shares = 0
 
     def rsiTrader(self):
-        self.current_price = indicators.get_current_price(indicators, "AMZN")
+        self.current_price = indicators.get_current_price(indicators, self.ticker)
         self.rsiData.append(self.current_price)
         
         if(len(self.rsiData) == 15):
@@ -68,38 +79,44 @@ class TradingAlgos():
             # calculate the RSI
 
             if len(gains) == 0:
-                RSI = 0
+                self.rsi = 0
             elif len(losses) == 0:
-                RSI = 100
+                self.rsi = 100
             else:
                 RS = (sum(gains) / len(gains)) / (sum(losses) / len(losses))
-                RSI = 100 - (100 / (1 + RS))
-            print(RSI)
+                self.rsi = 100 - (100 / (1 + RS))
+            print(f"RSI: {self.rsi}")
             # remove the first element from data
             self.rsiData.pop(0)
         else:
             pass
 
-        print(len(self.rsiData))
 
 
     def macdTrader(self):
-        self.current_price = indicators.get_current_price(indicators, "AMZN")
+        self.current_price = indicators.get_current_price(indicators, self.ticker)
         self.macdData.append(self.current_price)
 
         if len(self.macdData) == 26:
             twenty_six_ema = self.calculateEMA(self.macdData, 26, 25)
             twelve_ema = self.calculateEMA(self.macdData, 12, 11)
 
-            MACD = twelve_ema - twenty_six_ema
+            self.macd = twelve_ema - twenty_six_ema
 
-            print(MACD)
+            self.macdHistory.append(self.macd)
+            if len(self.macdHistory) > 8:
+                self.macdSignalLine = self.calculateEMA(self.macdHistory[-9:], 9, 8)
 
-        
+
+            print(f"MACD value: {self.macd}")
+            print(f"MACD signal: {self.macdSignalLine}")
+
+
+
+
             self.macdData.pop(0)
         else:
             pass
-        print(len(self.macdData))
 
     def calculateEMA(self, data, period, index):
 
@@ -110,22 +127,41 @@ class TradingAlgos():
         else:
             return (float(data[index]) * smoothing)+ (self.calculateEMA(data, period, index - 1) * (1-smoothing))
         
-    def stochastic(self):
+    def stoTrader(self):
 
 
-        self.current_price = indicators.get_current_price(indicators, "AMZN")
+        self.current_price = indicators.get_current_price(indicators, self.ticker)
         self.stochasticData.append(self.current_price)
             
         if(len(self.stochasticData) == 14):
             L14 = min(self.stochasticData)
             H14 = max(self.stochasticData)
-            K = 100 * ((self.current_price - L14)/(H14 - L14))
-            print(K)
+            self.stochasticK = 100 * ((self.current_price - L14)/(H14 - L14))
+            print(f"StochasticK: {self.stochasticK}")
             # remove the first element from data
             self.stochasticData.pop(0)
         else:
             pass   
-        print(len(self.stochasticData))
+
+
+    def tripleThreatTrader(self):
+        self.tripleThreatCounter += 1
+        self.macdTrader()
+        self.rsiTrader()
+        self.stoTrader()
+        print(self.tripleThreatCounter)
+
+        if(self.tripleThreatCounter > 35 and self.stochasticK < 20 and self.rsi > 50 and self.macd > self.macdSignalLine):
+            print("BUY")
+            self.alpacaTrader.buyStock(self.ticker, 1)
+
+
+        if (self.tripleThreatCounter > 35 and self.stochasticK > 80 and self.rsi < 50 and self.macd < self.macdSignalLine):
+            print("SELL")
+            self.alpacaTrader.sellStock(self.ticker, 1)
+
+
+
 
  
 
